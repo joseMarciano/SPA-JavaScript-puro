0392fe67efea4006b4abf866028b7d75377c6445
zezeka <marcianojosepaulo@gmail.com>
2020-11-07 13:05:36 -0300

    02. Conhecendo o Webpack | CAP - 07 Carregando a imagem

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,20 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.css$/i, //identificação de arquivos .css com REGEXP
-            use: ["style-loader", "css-loader"]
-        }]
+                test: /\.css$/i, //identificação de arquivos .css com REGEXP 
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"], // esse cara pra pegar as tags que tenha um src e converter em JS para importar o conteudo dentro de src uma tag img por exemplo
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: "file-loader", // para importar os arquivos de imagens e transforma-los em JS
+                options: { // opcional... colocar o nome do arquivo no bundle [name].[ext] diz que o nome será o nome original.extensaoDoArquivo
+                    name: "[name].[ext]"
+                }
+            }
+        ]
     }
 }
