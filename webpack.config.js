c3e305bc21571df554da7a9d8a65a834ea2e7eab
zezeka <marcianojosepaulo@gmail.com>
2020-11-07 11:22:45 -0300

    02. Conhecendo o Webpack | CAP - 04 Criando o devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/componentes/lista/listagem-cliente.js",
+    mode: "development", // moodo de desenvolvimento
+    devServer: {
+        open: true, // abrir automático
+        contentBase: "dist" // qual a pasta que o server vai olhar (olhando para o bundle gerado)
+    },
+    entry: "./src/componentes/lista/listagem-cliente.js", // ponto inicial que o webpack vai olhar
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "main.js", // nome do arquivo bundle que será gerado
+        path: path.resolve(__dirname, "dist") // pasta que será gerado o bundle
     }
 }
