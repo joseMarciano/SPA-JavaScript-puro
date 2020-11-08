27f2cf0a3ca2d53ce8e68566ec34fe8aac96af77
zezeka <marcianojosepaulo@gmail.com>
2020-11-08 14:40:12 -0300

    05. Finalizando os componentes | CAP - 02 Resolvendo a edição

--- src/componentes/edita/edita-cliente.js
+++ src/componentes/edita/edita-cliente.js
@@ -1,51 +1,53 @@
-const pegaURL = new URL(window.location)
+import { detalhaCliente, editaCliente } from '../../api/cliente.js';
+import validaCPF from '../valida/validaCPF.js';
 
-const id = pegaURL.searchParams.get('id')
 
-const inputCPF = document.querySelector('[data-cpf]')
-const inputNome = document.querySelector('[data-nome]')
+const eventoForm = (form) => {
+    const pegaURL = new URL(window.location)
 
-detalhaCliente(id).then( dados => {
-    inputCPF.value = dados[0].cpf 
-    inputNome.value = dados[0].nome
-})
+    const id = pegaURL.searchParams.get('id')
 
-const formEdicao = document.querySelector('[data-form]')
+    const inputCPF = document.querySelector('[data-cpf]')
+    const inputNome = document.querySelector('[data-nome]')
 
-const alerta = (classe, mensagem) => { 
-    const linha = document.createElement('tr');
+    detalhaCliente(id).then(dados => {
+        inputCPF.value = dados[0].cpf
+        inputNome.value = dados[0].nome
+    })
 
-    const conteudoLinha = `
+    const alerta = (classe, mensagem) => {
+        const linha = document.createElement('section');
+
+        const conteudoLinha = `
     <div class="${classe}">${mensagem}</div>
     
 `
-  
-    linha.innerHTML = conteudoLinha;
-    return linha;
-} 
-formEdicao.addEventListener('submit', event => { 
-    event.preventDefault()
 
-    if(!validaCPF(inputCPF.value)){
-        alert("ESSE NÃO EXISTE")
-        return 
+        linha.innerHTML = conteudoLinha;
+        return linha;
     }
+    form.addEventListener('submit', event => {
+        event.preventDefault()
 
-    editaCliente(id, inputCPF.value, inputNome.value)
-    .then( resposta => { 
-        if( resposta.status === 200){
-            formEdicao.appendChild(alerta(
-                "alert alert-success",
-                "CLIENTE EDITADO COM SUCESSO !"
-            ))
-        } else { 
-            formEdicao.appendChild(alerta(
-                "alert alert-warning",
-                "O CLIENTE NÃO PODE SER EDITADO !"
-            ))
+        if (!validaCPF(inputCPF.value)) {
+            alert("ESSE NÃO EXISTE")
+            return
         }
-    })
-    
-    
 
-})
+        editaCliente(id, inputCPF.value, inputNome.value)
+            .then(() => {
+                form.appendChild(alerta(
+                    "alert alert-success",
+                    "CLIENTE EDITADO COM SUCESSO !"
+                ));
+            })
+            .catch(() => {
+                form.appendChild(alerta(
+                    "alert alert-warning",
+                    "O CLIENTE NÃO PODE SER EDITADO !"
+                ));
+            })
+    })
+}
+
+export default eventoForm;
