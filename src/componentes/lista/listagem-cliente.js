a899633eb5b992baa318d4d910e1bf60926955ba
zezeka <marcianojosepaulo@gmail.com>
2020-11-08 14:05:34 -0300

    04. Criando o router | CAP - 04 Refatorando a listagem

--- src/componentes/lista/listagem-cliente.js
+++ src/componentes/lista/listagem-cliente.js
@@ -1,6 +1,5 @@
 import { deletaCliente, listarClientes } from '../../api/cliente.js';
 import "../../assets/css/clientes.css";
-import inicializaCadastro from '../cadastro/componente-cadastro.js';
 
 const removeCliente = (id) => {
     if (confirm("Deseja deletar o cliente ?")) {
@@ -10,14 +9,45 @@ const removeCliente = (id) => {
     }
 }
 
-const conteudo = `
+const criaCorpoTabela = (tabela) => {
+    const corpoTabela = document.createElement('tbody');
+
+    const exibeCliente = (cpf, nome, id) => {
+        const linha = document.createElement('tr');
+
+        const conteudoLinha = `
+        <td>${cpf}</td>
+        <td>${nome}</td>
+        <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
+        <a href="./componentes/edita/edita-clientes.html?id=${id}">
+        <button type="button" class="btn btn-info">Editar</button>
+        </a>   
+    `
+
+        linha.innerHTML = conteudoLinha;
+        return linha;
+    };
+
+    listarClientes().then(exibe => {
+        exibe.forEach(indice => {
+            corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+        })
+    })
+    tabela.appendChild(corpoTabela);
+
+
+}
+
+const inicializaTabela = () => {
+
+    const cabecalho = `
     <thead class="thead-dark">
         <tr>
             <th scope="col">CPF</th>
             <th scope="col">Nome</th>
             <th scope="col"></th>
             <th scope="col">
-                <a class="btn btn-primary">
+                <a onclick="navegacao('/cadastro'); return false;" class="btn btn-primary">
                     Novo cliente
                 </a>
             </th>
@@ -25,43 +55,13 @@ const conteudo = `
     </thead>
 
 `;
-const container = document.querySelector('[data-container]');
-const tabela = document.createElement('table');
-tabela.innerHTML = conteudo;
-tabela.classList.add("table");
+    const tabela = document.createElement('table');
+    tabela.innerHTML = cabecalho;
+    tabela.classList.add("table");
 
-container.appendChild(tabela);
-const novoClienteButtom = document.querySelector('.btn');
-debugger
+    criaCorpoTabela(tabela);
 
-novoClienteButtom.addEventListener('click', () => {
-    debugger
-    inicializaCadastro();
-})
+    return tabela;
+}
 
-const corpoTabela = document.createElement('tbody');
-
-const exibeCliente = (cpf, nome, id) => {
-    const linha = document.createElement('tr');
-
-    const conteudoLinha = `
-    <td>${cpf}</td>
-    <td>${nome}</td>
-    <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
-    <a href="./componentes/edita/edita-clientes.html?id=${id}">
-    <button type="button" class="btn btn-info">Editar</button>
-    </a>   
-`
-
-    linha.innerHTML = conteudoLinha;
-    return linha;
-};
-
-listarClientes().then(exibe => {
-        exibe.forEach(indice => {
-            corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
-        })
-    }
-
-)
-tabela.appendChild(corpoTabela);
+export default inicializaTabela;
